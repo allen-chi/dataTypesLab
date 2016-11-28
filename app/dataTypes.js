3a85a8c4d61c18136497f547d4d86158cd197c30
Ems <Ems>
2016-11-28 15:59:27 +0100

    updated

--- app/dataTypes.js
+++ app/dataTypes.js
@@ -9,9 +9,21 @@ module.exports ={
 			return myVar;
 		}
 
-		if (typeof myVar === 'number'){
+		if (typeof myVar === "number"){
 			if (myVar < 100){
 				return "less than 100";
+			}
+			else if (myVar > 100){
+				return "more than 100";
+			}
+			else if (myVar === 100){
+				return "equal to 100";
+			}
+		}
+
+		if (typeof myVar === "string"){
+			if (myVar || myVar === null || !myVar){
+				return myVar.length;
 			}
 		}
 
