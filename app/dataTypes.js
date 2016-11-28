591a35f31f60d04aa1ebb60816664459de290599
Ems <Ems>
2016-11-28 15:41:53 +0100

    updated

--- app/dataTypes.js
+++ app/dataTypes.js
@@ -1,1 +1,19 @@
+module.exports ={
+	dataTypes:function(myVar){
 
+		if (myVar ===null || myVar === undefined){
+			return "no value";
+		}
+
+		if ( myVar === true || myVar === false){
+			return myVar;
+		}
+
+		if (typeof myVar === 'number'){
+			if (myVar < 100){
+				return "less than 100";
+			}
+		}
+
+	}
+}
